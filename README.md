3bf32bca10d28ed030928c35f8be7ca7ec991a46
Rob3rto-Lira <rliraleon@gmail.com>
2024-12-21 14:23:52 -0300

    Finish a working JS code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Introduccion-JS
+## Español
+En este repositorio, estoy probando JavaScript con un proyecto pequeño.
+## English
+In this repository, I'm testing the waters of Javascript with a small little project.
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+cantidad = document.querySelector(".cantidad").innerHTML;
+
+
+function mas() {
+    ++cantidad;
+    valor = document.querySelector(".cantidad").innerHTML = cantidad;
+
+    total = valor;
+    pagar = total.innerHTML;
+    pagar = precio * valor;
+    document.querySelector(".valor-total").innerHTML = pagar;
+}
+
+function menos() {
+    --cantidad;
+    document.querySelector(".cantidad").innerHTML = cantidad;
+
+    total = document.querySelector(".valor-total").innerHTML;
+    document.querySelector(".valor-total").innerHTML = total - precio;
+}
